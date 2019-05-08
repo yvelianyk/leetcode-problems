5d7425da7368c21f90b407915d225864890aaba7
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-08 23:18:27 +0300

    [94] Binary Tree Inorder Traversal
    - recursivelly and iterative solutions

--- src/com/company/BinaryTreeInorderTraversal94/BinaryTreeInorderTraversal.java
+++ src/com/company/BinaryTreeInorderTraversal94/BinaryTreeInorderTraversal.java
@@ -4,23 +4,45 @@ import javax.swing.tree.TreeNode;
 import java.util.*;
 
 public class BinaryTreeInorderTraversal {
+
+    // =========================================================
+    // ITERATIVE(DFS):
+    // =========================================================
+
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        Stack<TreeNode> stack = new Stack<>();
+//        ArrayList<Integer> result = new ArrayList<Integer>();
+//        TreeNode current = root;
+//
+//        while (current != null || !stack.isEmpty()) {
+//            while (current.left != null) {
+//                stack.push(current.left);
+//                current = current.left;
+//            }
+//
+//            current = stack.pop();
+//            result.add(current.val);
+//            current = current.right;
+//        }
+//
+//        return result;
+//    }
+
+    // =========================================================
+    // RECURSIVE:
+    // =========================================================
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
         ArrayList<Integer> result = new ArrayList<Integer>();
-        TreeNode current = root;
-
-        while (current != null || !stack.isEmpty()) {
-            while (current.left != null) {
-                stack.push(current.left);
-                current = current.left;
-            }
-
-            current = stack.pop();
-            result.add(current.val);
-            current = current.right;
-        }
-
+        if(root == null) return result;
+        traverse(root, result);
         return result;
+    }
+
+    private void traverse(TreeNode node, ArrayList<Integer> result) {
+        if(node.left != null) traverse(node.left, result);
+        result.add(node.val);
+        if(node.right != null) traverse(node.right, result);
     }
 
     public class TreeNode {
