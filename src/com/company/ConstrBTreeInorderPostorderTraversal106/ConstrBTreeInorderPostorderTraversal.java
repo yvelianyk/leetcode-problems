31e5e3ba600baf7f2e2e191c6be6b6963d84a734
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-12 12:26:54 +0300

    [106] Construct Binary Tree from Postorder and Inorder Traversal
    - can be optimized but still O(n) current solution

--- src/com/company/ConstrBTreeInorderPostorderTraversal106/ConstrBTreeInorderPostorderTraversal.java
+++ src/com/company/ConstrBTreeInorderPostorderTraversal106/ConstrBTreeInorderPostorderTraversal.java
@@ -7,30 +7,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// CAN BE OPTIMIZED BY NOT USING ARRAY LISTS AND PASSING INDICES
 public class ConstrBTreeInorderPostorderTraversal {
-    private Map<Integer, Integer> inorderMap = new HashMap<>();
-
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        List<Integer> postorderList = new ArrayList<>();
+        List<Integer> inorderList = new ArrayList<>();
+        if (postorder.length == 0 && inorder.length == 0) return null;
 
-        for (int i = 0; i < inorder.length; i++) inorderMap.put(inorder[i], i);
-        TreeNode root = new TreeNode(postorder[0]);
+        for (int i = 0; i < inorder.length; i++) inorderList.add(inorder[i]);
+        for (int i = 0; i < postorder.length; i++) postorderList.add(postorder[i]);
 
-        List<Integer> leftInorder = new ArrayList<>();
-        List<Integer> leftPostOrder = new ArrayList<>();
-        List<Integer> rightInorder = new ArrayList<>();
-        List<Integer> rightPostOrder = new ArrayList<>();
-
-        for (int i = 0; i < inorderMap.get(root.val); i++) {
-            leftInorder.add(inorder[i]);
-        }
-        for (int i = inorderMap.get(root.val) + 1; i < inorder.length; i++) {
-            rightInorder.add(inorder[i]);
-        }
-
-        return new TreeNode(1);
+        return traverse(inorderList, postorderList);
     }
 
-//    private TreeNode traverse(List<Integer> inorder, List<Integer> postorder) {
-//
-//    }
+    private TreeNode traverse(List<Integer> inorder, List<Integer> postorder) {
+        Map<Integer, Integer> inorderMap = new HashMap<>();
+
+        for (int i = 0; i < inorder.size(); i++) inorderMap.put(inorder.get(i), i);
+        TreeNode root = new TreeNode(postorder.get(postorder.size() - 1));
+
+        List<Integer> leftInorder = new ArrayList<>();
+        List<Integer> leftPostorder = new ArrayList<>();
+        List<Integer> rightInorder = new ArrayList<>();
+        List<Integer> rigthPostorder = new ArrayList<>();
+        for (int i = 0; i < inorderMap.get(root.val); i++) leftInorder.add(inorder.get(i));
+        for (int i = inorderMap.get(root.val) + 1; i < inorder.size(); i++) rightInorder.add(inorder.get(i));
+        for (int i = 0; i < leftInorder.size(); i++) leftPostorder.add(postorder.get(i));
+        for (int i = leftInorder.size(); i < postorder.size() - 1; i++) rigthPostorder.add(postorder.get(i));
+
+        if (leftInorder.size() > 1) root.left = traverse(leftInorder, leftPostorder);
+        if (rightInorder.size() > 1) root.right = traverse(rightInorder, rigthPostorder);
+        if (leftInorder.size() == 1) root.left = new TreeNode(leftInorder.get(0));
+        if (rightInorder.size() == 1) root.right = new TreeNode(rightInorder.get(0));
+
+        return root;
+    }
+
+
 }
