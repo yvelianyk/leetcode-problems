d314c9054c9e45798539feddf473e20ea92207c1
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-18 21:50:46 +0300

    [278] first bad version

---
+++ src/com/company/FirstBadVersion278/FirstBadVersion.java
@@ -0,0 +1,23 @@
+package com.company.FirstBadVersion278;
+
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private boolean isBadVersion(int version) {
+        return version == 2 || version == 3 || version == 4;
+    }
+}
