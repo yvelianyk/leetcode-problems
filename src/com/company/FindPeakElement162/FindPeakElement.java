a0bfcf84c38ff7f7a6c2591a24976dc0123026cf
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-19 09:22:23 +0300

    [162] find peak element improved

--- src/com/company/FindPeakElement162/FindPeakElement.java
+++ src/com/company/FindPeakElement162/FindPeakElement.java
@@ -5,18 +5,16 @@ public class FindPeakElement {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            double midLeft = mid == 0 ? Double.NEGATIVE_INFINITY : nums[mid - 1];
-            double midRight = mid == nums.length - 1 ? Double.NEGATIVE_INFINITY : nums[mid + 1];
-            if (nums[mid] > midLeft && nums[mid] > midRight) return mid;
-            if (midRight > nums[mid]) {
+            int midRight = mid + 1;
+            if (nums[midRight] > nums[mid]) {
                 left = mid + 1;
             } else {
-                right = mid - 1;
+                right = mid;
             }
         }
 
-        return -1;
+        return left;
     }
 }
