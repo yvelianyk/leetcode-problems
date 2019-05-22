b1ac643daf97eba6fbcc1b4e9ab6618514121cca
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-22 19:31:21 +0300

    [162] find peak element, template 3

--- src/com/company/FindPeakElement162/FindPeakElement.java
+++ src/com/company/FindPeakElement162/FindPeakElement.java
@@ -1,20 +1,40 @@
 package com.company.FindPeakElement162;
 
 public class FindPeakElement {
+
+    // TEMPLATE 3
     public int findPeakElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
+        while (left + 1 < right) {
             int mid = left + (right - left) / 2;
-            int midRight = mid + 1;
-            if (nums[midRight] > nums[mid]) {
-                left = mid + 1;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid;
             } else {
                 right = mid;
             }
         }
 
-        return left;
+        return nums[left] < nums[right] ? right : left;
+
     }
+
+    // TEMPLATE 1
+//    public int findPeakElement(int[] nums) {
+//        int left = 0;
+//        int right = nums.length - 1;
+//
+//        while (left < right) {
+//            int mid = left + (right - left) / 2;
+//            int midRight = mid + 1;
+//            if (nums[midRight] > nums[mid]) {
+//                left = mid + 1;
+//            } else {
+//                right = mid;
+//            }
+//        }
+//
+//        return left;
+//    }
 }
