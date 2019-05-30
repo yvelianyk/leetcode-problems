4f77452e465713a3cb9dab0621ea9aba80a35be4
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-05-30 23:30:10 +0300

    [98] validate BST

---
+++ src/com/company/ValidateBinarySearchTree98/ValidateBinarySearchTree.java
@@ -0,0 +1,32 @@
+package com.company.ValidateBinarySearchTree98;
+
+import com.company.BinaryTreePostorderTraversal145.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+        List<Integer> list = new ArrayList<>();
+        traverse(root, list);
+        if (list.size() == 1) return true;
+
+        for (int i = 1; i < list.size(); i++) {
+             int prev = list.get(i - 1);
+             int curr = list.get(i);
+             if (curr <= prev) return false;
+
+        }
+        return true;
+    }
+
+    private void traverse(TreeNode root, List list) {
+        if (root == null) return;
+        traverse(root.left, list);
+        list.add(root.val);
+        traverse(root.right, list);
+    }
+}
