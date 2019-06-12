4e125738c4fd5c5d1ad75a9ee04ef526774e452b
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-06-12 22:29:07 +0300

    [344] reverse string recursivelly

--- src/com/company/ReverseString344/ReverseString.java
+++ src/com/company/ReverseString344/ReverseString.java
@@ -1,15 +1,32 @@
 package com.company.ReverseString344;
 
 public class ReverseString {
+
     public void reverseString(char[] s) {
-        int i = 0;
-        int j = s.length - 1;
-        while (i < j) {
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
-            i++;
-            j--;
-        }
+        traverse(0, s);
     }
+
+    private void traverse(int index, char[] chars) {
+        if (index >= chars.length / 2) return;
+
+        int endIndex = chars.length - 1 - index;
+        char temp = chars[index];
+        chars[index] = chars[endIndex];
+        chars[endIndex] = temp;
+
+        traverse(index + 1, chars);
+    }
+
+    // ITERATIVE:
+//    public void reverseString(char[] s) {
+//        int i = 0;
+//        int j = s.length - 1;
+//        while (i < j) {
+//            char temp = s[i];
+//            s[i] = s[j];
+//            s[j] = temp;
+//            i++;
+//            j--;
+//        }
+//    }
 }
