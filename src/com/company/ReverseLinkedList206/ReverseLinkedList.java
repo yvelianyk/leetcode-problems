c973ea63aed3ec185822d7c1a212542d1db90914
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-06-14 22:44:14 +0300

    [206] reverse linked list recursivelly

--- src/com/company/ReverseLinkedList206/ReverseLinkedList.java
+++ src/com/company/ReverseLinkedList206/ReverseLinkedList.java
@@ -3,41 +3,35 @@ package com.company.ReverseLinkedList206;
 import com.company.LinkedListCycleII142.ListNode;
 
 public class ReverseLinkedList {
-    public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return head;
-        ListNode node = head;
-        while(node.next != null) {
-            ListNode next = node.next;
-            node.next = next.next;
-            next.next = head;
-            head = next;
-        }
-        return head;
-    }
+
+    // ITERATIVELLY:
+    //    public ListNode reverseList(ListNode head) {
+//        if(head == null) return null;
+//        if(head.next == null) return head;
+//        ListNode node = head;
+//        while(node.next != null) {
+//            ListNode next = node.next;
+//            node.next = next.next;
+//            next.next = head;
+//            head = next;
+//        }
+//        return head;
+//    }
 
 
 
 
 // RECURSIVELLY
-//    private ListNode head;
-//
-//    public ListNode reverseList(ListNode head) {
-//        if(head == null) return null;
-//        if(head.next == null) return head;
-//        this.head = head;
-//        traverse(head);
-//        return this.head;
-//    }
-//
-//    private void traverse(ListNode node) {
-//        if(node.next == null) return;
-//        ListNode next = node.next;
-//        node.next = next.next;
-//        next.next = head;
-//        head = next;
-//        traverse(node);
-//    }
+
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
 
 
 
