c8fbab2f09a2d6c4e1f1f5ea336f75b8731403c6
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-06-18 08:43:55 +0300

    [652] find duplicated subtrees in the binary tree

---
+++ src/com/company/FindDuplicatedSubtrees652/FindDuplicatedSubtrees.java
@@ -0,0 +1,30 @@
+package com.company.FindDuplicatedSubtrees652;
+
+import com.company.BinaryTreePostorderTraversal145.TreeNode;
+
+import java.util.*;
+
+public class FindDuplicatedSubtrees {
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        List<TreeNode> result = new ArrayList<>();
+        if (root == null) return result;
+        Map<String, Integer> nodesMap = new HashMap<>();
+
+        traverse(root, nodesMap, result);
+
+        return result;
+    }
+
+    private String traverse(TreeNode node, Map<String, Integer> nodesMap, List<TreeNode> result) {
+        if (node == null) return "#";
+        String serialized = node.val + "," + traverse(node.left, nodesMap, result) +
+        "," + traverse(node.right, nodesMap, result);
+
+        nodesMap.put(serialized, nodesMap.getOrDefault(serialized, 0) + 1);
+        if (nodesMap.get(serialized) == 2) {
+            result.add(node);
+        }
+        return serialized;
+    }
+
+}
