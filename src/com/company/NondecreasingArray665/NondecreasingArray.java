5cd92339d4f48413d88fe10ce2603ab2c243ca92
Yaroslav Velianyk <yvelianyk@softjourn.com>
2019-06-25 09:22:08 +0300

    [665] non decreasing array

--- src/com/company/NondecreasingArray665/NondecreasingArray.java
+++ src/com/company/NondecreasingArray665/NondecreasingArray.java
@@ -2,14 +2,13 @@ package com.company.NondecreasingArray665;
 
 public class NondecreasingArray {
     public boolean checkPossibility(int[] nums) {
-        if(nums == null || nums.length == 1) return true;
+        if (nums == null || nums.length == 1) return true;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] > nums[i + 1]) {
-                int first = nums[i];
-                int second = nums[i + 1];
-                boolean check1 = check(nums, i, i + 1, first);
-                nums[i+1] = second;
-                boolean check2 = check(nums, i - 1, i, second);
+                int temp = nums[i + 1];
+                boolean check1 = check(nums, i, i + 1, nums[i]);
+                nums[i + 1] = temp;
+                boolean check2 = check(nums, i - 1, i, nums[i + 1]);
                 return check1 || check2;
             }
         }
@@ -17,9 +16,8 @@ public class NondecreasingArray {
     }
 
     private boolean check(int[] nums, int start, int index, int number) {
-        if (start == nums.length - 1) return true;
-        nums[index] = number;
         if (start < 0) start = 0;
+        nums[index] = number;
         for (int i = start; i < nums.length - 1; i++) {
             if (nums[i] > nums[i + 1]) return false;
         }
