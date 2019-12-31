339f3995f55af8c612e318cfc6e6c9995e514727
yvelianyk <yvelianyk@prismhr.com>
2019-12-31 16:21:02 +0200

    feat: critical connections in the network. Trajan algorithm. Need to improve.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,15 +1,42 @@
 package com.company;
 
-import com.company.Contest169.Problem3;
+import com.company.MockInterviews.AmazonMockInterviews.Mock12272019.Problem2;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    // 6
+    //[[0,1],[1,2],[2,0],[1,3],[3,4],[4,5],[5,3]]
     public static void main(String[] args) throws IOException {
+        Problem2 solution = new Problem2();
 
-        Problem3 solution = new Problem3();
-        boolean result = solution.canReach(new int[]{4,2,3,0,3,1,2}, 5);
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(0);
+        list1.add(1);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(2);
+        list3.add(0);
+
+        List<Integer> list4 = new ArrayList<>();
+        list4.add(1);
+        list4.add(3);
+
+        List<List<Integer>> forest = new ArrayList<>();
+        forest.add(list1);
+        forest.add(list2);
+        forest.add(list3);
+        forest.add(list4);
+
+
+        List<List<Integer>> result = solution.criticalConnections(4, forest);
         System.out.println(result);
 
     }
--- src/main/java/com/company/MockInterviews/AmazonMockInterviews/Mock12272019/Problem2.java
+++ src/main/java/com/company/MockInterviews/AmazonMockInterviews/Mock12272019/Problem2.java
@@ -3,28 +3,69 @@ package com.company.MockInterviews.AmazonMockInterviews.Mock12272019;
 import java.util.*;
 
 // https://leetcode.com/problems/critical-connections-in-a-network/
-// TODO: figure out how to find if some node in the graph in the cycle or not
 public class Problem2 {
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        Map<Integer, Node> graph = new HashMap<>();
         List<List<Integer>> result = new ArrayList<>();
+        buildGraph(connections, graph);
 
-        for (int i = 0; i < connections.size(); i++) {
-            int first = connections.get(i).get(0);
-            int second = connections.get(i).get(1);
+        Node root = graph.get(connections.get(0).get(0));
 
-            List<Integer> firstAdj = graph.getOrDefault(first, new ArrayList<>());
-            firstAdj.add(second);
-            graph.put(first, firstAdj);
-            List<Integer> secondAdj = graph.getOrDefault(second, new ArrayList<>());
-            secondAdj.add(first);
-            graph.put(second, secondAdj);
-        }
+        int[] disc = new int[n];
+        int[] low = new int[n];
+        int[] visited = new int[n];
 
-
-        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
-            List<Integer> entryConnections = entry.getValue();
-        }
+        DFS(root, disc, low, -1, visited, 0, result);
 
         return result;
-    }}
+    }
+
+    private void DFS(Node node, int[] disc, int[] low, int parent, int[] visited, int counter, List<List<Integer>> result) {
+        disc[node.number] = counter + 1;
+        low[node.number] = counter + 1;
+        visited[node.number] = 1;
+
+        for (Node child : node.children) {
+            if (child.number == parent) {
+                continue;
+            }
+            if (visited[child.number] == 0) {
+                DFS(child, disc, low, node.number, visited, ++counter, result);
+                low[node.number] = Math.min(low[node.number], low[child.number]);
+                if(low[child.number] > disc[node.number]) {
+                    List<Integer> res = new ArrayList<>();
+                    res.add(node.number);
+                    res.add(child.number);
+                    result.add(res);
+                }
+            } else {
+                low[node.number] = Math.min(low[node.number], disc[child.number]);
+            }
+        }
+    }
+
+    private void buildGraph(List<List<Integer>> connections, Map<Integer, Node> graph) {
+        for (List<Integer> connection : connections) {
+            int first = connection.get(0);
+            int second = connection.get(1);
+
+            Node firstNode = graph.getOrDefault(first, new Node(first));
+            Node secondNode = graph.getOrDefault(second, new Node(second));
+
+            firstNode.children.add(secondNode);
+            secondNode.children.add(firstNode);
+            graph.put(first, firstNode);
+            graph.put(second, secondNode);
+        }
+    }
+
+    private class Node {
+        List<Node> children;
+        int number;
+
+        Node(int number) {
+            this.number = number;
+            this.children = new ArrayList<>();
+        }
+    }
+}
