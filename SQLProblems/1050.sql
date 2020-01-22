9283552d8798dec69e60e166961eadc4f2059655
yvelianyk <yvelianyk@prismhr.com>
2020-01-22 21:42:41 +0200

    feat: actors and directors three times appears(1050)

---
+++ SQLProblems/1050.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+select actor_id, director_id from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3
