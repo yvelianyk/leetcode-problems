794213c1e8e850d076d3b229b84cc7d7f4da36f1
yvelianyk <yvelianyk@prismhr.com>
2020-05-01 18:59:13 +0300

    feat: [325] maximum size subarray equals k

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,23 +1,19 @@
 package com.company;
 
 
-import com.company.Contest186.Problem1;
-import com.company.Contest186.Problem2;
-import com.company.Contest186.Problem4;
-import com.company.Contest186.Problem4BottomUp;
 import com.company.MakeArrayStrictlyIncreasing1187.MakeArrayStrictlyIncreasing;
-import com.company.ThirtyDayChellenge.FirstUnique;
+import com.company.MaximumSizeSubarraySumEqualsK325.MaximumSizeSubarraySumEqualsK;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        MakeArrayStrictlyIncreasing solution = new MakeArrayStrictlyIncreasing();
+        MaximumSizeSubarraySumEqualsK solution = new MaximumSizeSubarraySumEqualsK();
 
         // [1,5,3,6,7]
         //[1,6,3,3]
-        int result = solution.makeArrayIncreasing(new int[]{1,5,3,6,7}, new int[]{1,6,3,3});
+        int result = solution.maxSubArrayLen(new int[]{1, -1, 5, -2, 3}, 3);
         System.out.println(result);
     }
 }
---
+++ src/main/java/com/company/MaximumSizeSubarraySumEqualsK325/MaximumSizeSubarraySumEqualsK.java
@@ -0,0 +1,37 @@
+package com.company.MaximumSizeSubarraySumEqualsK325;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumSizeSubarraySumEqualsK {
+//    public int maxSubArrayLen(int[] nums, int k) {
+//        int result = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            int inter = 0;
+//            for (int j = i; j < nums.length; j++) {
+//                inter += nums[j];
+//                if (inter == k) {
+//                    result = Math.max(result, j - i + 1);
+//                }
+//            }
+//        }
+//        return result;
+//    }
+    public int maxSubArrayLen(int[] nums, int k) {
+        int result = 0;
+        int[] prefix = new int[nums.length + 1];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < prefix.length; i++) {
+            int num = prefix[i] - k;
+            if (map.containsKey(num)) {
+                result = Math.max(result, i - map.get(num));
+            }
+            map.putIfAbsent(prefix[i], i);
+        }
+        return result;
+    }
+}
