c184d1c8c4adf171f6d27c4f946c369249c1c07c
yvelianyk <yvelianyk@prismhr.com>
2020-06-11 17:51:45 +0300

    fix: [1220] Count vowels permutation improve performance a little bit

--- src/main/java/com/company/CountVowelsPermutation1220/CountVowelsPermutation.java
+++ src/main/java/com/company/CountVowelsPermutation1220/CountVowelsPermutation.java
@@ -3,6 +3,7 @@ package com.company.CountVowelsPermutation1220;
 import java.util.Arrays;
 
 public class CountVowelsPermutation {
+
     private final char[] vowels = new char[]{'#','a', 'e', 'i', 'o', 'u'};
     private final int MOD = 1_000_000_007;
     private int length;
@@ -10,7 +11,7 @@ public class CountVowelsPermutation {
 
     public int countVowelPermutation(int n) {
         length = n;
-        memo = new int[n + 1][6];
+        memo = new int[n][6];
         for (int[] ints : memo) Arrays.fill(ints, -1);
         return count(0, 0);
     }
@@ -21,24 +22,24 @@ public class CountVowelsPermutation {
 
         long result = 0;
         for (int i = 1; i < vowels.length; i++) {
-            char vowel = vowels[i];
+            char curVowel = vowels[i];
             switch (vowels[previous]) {
                 case 'a':
-                    if (vowel == 'e') result = (result + count(index + 1, i)) % MOD;
+                    if (curVowel == 'e') result = (result + count(index + 1, i)) % MOD;
                     break;
                 case 'e':
-                    if (vowel == 'a' || vowel == 'i')
+                    if (curVowel == 'a' || curVowel == 'i')
                         result = (result + count(index + 1, i)) % MOD;
                     break;
                 case 'i':
-                    if (vowel != 'i') result = (result + count(index + 1, i)) % MOD;
+                    if (curVowel != 'i') result = (result + count(index + 1, i)) % MOD;
                     break;
                 case 'o':
-                    if (vowel == 'i' || vowel == 'u')
+                    if (curVowel == 'i' || curVowel == 'u')
                         result = (result + count(index + 1, i)) % MOD;
                     break;
                 case 'u':
-                    if (vowel == 'a') result = (result + count(index + 1, i)) % MOD;
+                    if (curVowel == 'a') result = (result + count(index + 1, i)) % MOD;
                     break;
                 default:
                     result = (result + count(index + 1, i)) % MOD;
