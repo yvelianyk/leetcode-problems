f1d32c4ea66f7f0e2eeaf3874b1dd5cb41e4011b
yvelianyk <yvelianyk@prismhr.com>
2020-06-16 19:38:26 +0300

    feat: [Contest193] solved 3. Easy when you know how to solve it :-)

--- src/main/java/com/company/Contest193/Problem3.java
+++ src/main/java/com/company/Contest193/Problem3.java
@@ -1,11 +1,51 @@
 package com.company.Contest193;
 
-// TODO:
 public class Problem3 {
+
+    int[] bloomDay;
+    int m;
+    int k;
+
     public int minDays(int[] bloomDay, int m, int k) {
         long flowers = m * k;
-        if (flowers < bloomDay.length) return -1;
+        if (flowers > bloomDay.length) return -1;
+        this.bloomDay = bloomDay;
+        this.m = m;
+        this.k = k;
 
-        return -1;
+        int lo = Integer.MAX_VALUE;
+        int hi = Integer.MIN_VALUE;
+        for (int i1 : bloomDay) {
+            lo = Math.min(lo, i1);
+            hi = Math.max(hi, i1);
+        }
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (check(mid)) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    private boolean check(int day) {
+        int adj = 0;
+        int number = 0;
+        for (int i1 : bloomDay) {
+            if (i1 <= day) {
+                adj++;
+                if (adj == k) {
+                    number++;
+                    adj = 0;
+                }
+            } else {
+                adj = 0;
+            }
+        }
+        return number >= m;
     }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.Contest193.Problem2;
+import com.company.Contest193.Problem3;
 import com.company.Contest193.TreeAncestor;
 
 import java.io.IOException;
@@ -8,10 +9,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        TreeAncestor treeAncestor = new TreeAncestor(7, new int[]{-1, 0, 0, 1, 1, 2, 2});
-        int res1 = treeAncestor.getKthAncestor(3, 1);  // returns 1 which is the parent of 3
-        int res2 = treeAncestor.getKthAncestor(5, 2);  // returns 0 which is the grandparent of 5
-        int res3 = treeAncestor.getKthAncestor(6, 3);  // returns -1 because there is no such ancestor
-        // System.out.println(res);
+        Problem3 treeAncestor = new Problem3();
+        int res = treeAncestor.minDays(new int[]{7,7,7,7,12,7,7}, 2, 3);
+        System.out.println(res);
     }
 }
