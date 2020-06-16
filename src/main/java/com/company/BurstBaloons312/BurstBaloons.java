88648a6b605913f9b9c5d27c26bb6237da4d17dc
yvelianyk <yvelianyk@prismhr.com>
2020-06-16 22:47:43 +0300

    feat: [312] Burst balloons

--- src/main/java/com/company/BurstBaloons312/BurstBaloons.java
+++ src/main/java/com/company/BurstBaloons312/BurstBaloons.java
@@ -4,13 +4,12 @@ package com.company.BurstBaloons312;
 public class BurstBaloons {
     int[][] memo;
     int[] nums;
+
     public int maxCoins(int[] nums) {
         int n = nums.length + 2;
         int[] new_nums = new int[n];
 
-        for(int i = 0; i < nums.length; i++){
-            new_nums[i+1] = nums[i];
-        }
+        for (int i = 0; i < nums.length; i++) new_nums[i + 1] = nums[i];
 
         new_nums[0] = new_nums[n - 1] = 1;
         this.nums = new_nums;
@@ -20,6 +19,7 @@ public class BurstBaloons {
 
     private int getMax(int left, int right) {
         if (left + 1 == right) return 0;
+        if (memo[left][right] > 0) return memo[left][right];
         int result = Integer.MIN_VALUE / 2;
         for (int i = left + 1; i < right; i++) {
             int leftMax = getMax(left, i);
@@ -27,6 +27,7 @@ public class BurstBaloons {
             int current = nums[left] * nums[i] * nums[right];
             result = Math.max(result, leftMax + rightMax + current);
         }
+        memo[left][right] = result;
         return result;
     }
 }
