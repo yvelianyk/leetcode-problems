58d518b4180a38c990c3e3bae43c95377b7549c8
yvelianyk-rigup <yaroslav.velianyk@rigup.co>
2020-12-18 11:46:58 +0200

    feat: four Sum II[Challenge]

--- src/main/java/com/company/Challenges/FourSumII.java
+++ src/main/java/com/company/Challenges/FourSumII.java
@@ -1,2 +1,30 @@
-package com.company.Challenges;public class FourSumII {
+package com.company.Challenges;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int n = A.length, result = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = C[i] + D[j];
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = A[i] + B[j];
+                if (map.containsKey(-sum))
+                    result += map.get(-sum);
+            }
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,25 +1,15 @@
 package com.company;
 
-import com.company.BContest41.Problem1;
-import com.company.BContest41.Problem2;
-import com.company.BContest41.Problem3;
-import com.company.BinaryTreePostorderTraversal145.TreeNode;
-import com.company.Challenges.NumPairsDivisibleby60;
-import com.company.Challenges.PalindromePartitioning;
-import com.company.Challenges.SubtreeWithAllDeepest;
-import com.company.Challenges.ValidMountainArray;
-import com.company.SerializeandDeserializeBinaryTree297.SerializeandDeserializeBinaryTree;
+import com.company.Challenges.FourSumII;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        NumPairsDivisibleby60 solution = new NumPairsDivisibleby60();
+      FourSumII solution = new FourSumII();
 
-        int res = solution.numPairsDivisibleBy60(new int[]{30,20, 150,100,40});
-       // int res = solution.numPairsDivisibleBy60(new int[]{20, 40});
+        int res = solution.fourSumCount(new int[]{0,1,-1}, new int[]{-1,1,0}, new int[]{0,0,1}, new int[]{-1,1,1});
         System.out.println(res);
     }
 }
