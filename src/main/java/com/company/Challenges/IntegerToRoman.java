64f2d5390701ea355d2cd4a551e52e0347530d86
yvelianyk-rigup <yaroslav.velianyk@rigup.co>
2021-03-10 17:05:58 +0200

    feat: integer to Roman [Challenge]

---
+++ src/main/java/com/company/Challenges/IntegerToRoman.java
@@ -0,0 +1,33 @@
+package com.company.Challenges;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class IntegerToRoman {
+    public static TreeMap<Integer, String> map = new TreeMap<>();
+    public String intToRoman(int num) {
+        map.put(1,"I");
+        map.put(5, "V");
+        map.put(10, "X");
+        map.put(50, "L");
+        map.put(100, "C");
+        map.put(500, "D");
+        map.put(1000, "M");
+        map.put(4, "IV");
+        map.put(9, "IX");
+        map.put(40, "XL");
+        map.put(90, "XC");
+        map.put(400, "CD");
+        map.put(900, "CM");
+
+        StringBuilder result = new StringBuilder();
+        while (num != 0) {
+            Map.Entry<Integer, String> entry = map.floorEntry(num);
+            result.append(entry.getValue());
+            num -= entry.getKey();
+        }
+        return result.toString();
+
+    }
+
+}
--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,19 +1,15 @@
 package com.company;
 
-import com.company.BinaryTreePostorderTraversal145.TreeNode;
-import com.company.Challenges.AddOneRowToTree;
-import com.company.SerializeandDeserializeBinaryTree297.SerializeandDeserializeBinaryTree;
+import com.company.Challenges.IntegerToRoman;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        AddOneRowToTree solution = new AddOneRowToTree();
-        SerializeandDeserializeBinaryTree serializer = new SerializeandDeserializeBinaryTree();
-        TreeNode root = serializer.deserialize("[4,2,6,3,1,5]");
+        IntegerToRoman solution = new IntegerToRoman();
 
-        TreeNode result = solution.addOneRow(root, 1, 2);
+        String result = solution.intToRoman(2998);
 
         System.out.println(result);
     }
